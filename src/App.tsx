58fd7fb5131a92d0e7a63eb0732f28986d1f5138
Justin Clark <jclarksummit@gmail.com>
2024-02-19 17:22:14 -0500

    Migrate App.tsx from next-refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import NavBar from "./components/NavBar";
+import useLocalStorage from "./hooks/useLocalStorage";
+import { JobHunterAPI } from "./api/api";
+import UserContext from "./auth/UserContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentUser, setCurrentUser] = useState({
+    data: null,
+    infoLoaded: false,
+  });
+  const [token, setToken] = useLocalStorage(TOKEN_STORAGE_KEY);
+
+  useEffect(
+    function fetchUserOnTokenChange() {
+      async function fetchCurrentUser() {
+        if (token) {
+          try {
+            const decoded = jwtDecode<DecodedToken>(token);
+            const username = decoded.username;
+            JobHunterAPI.token = token;
+            let currentUser = await JobHunterAPI.getUser(username);
+
+            setCurrentUser({
+              infoLoaded: true,
+              data: currentUser,
+            });
+          } catch (err) {
+            console.error(
+              "App fetchUserOnTokenChange: problem fetching user",
+              err
+            );
+            setCurrentUser({
+              infoLoaded: true,
+              data: null,
+            });
+          }
+        }
+      }
+      fetchCurrentUser();
+    },
+    [token]
+  );
+
+  // Logging in and out
+  /** Handles log out */
+  const logout = () => {
+    setCurrentUser({
+      infoLoaded: false,
+      data: null,
+    });
+    setToken(null);
+  };
+
+  /** Handles log in */
+  const login = async (loginData: LoginData) => {
+    let token = await JobHunterAPI.loginUser(loginData);
+    setToken(token);
+  };
+
+  const signup = async (signupData: SignUpData) => {
+    let token = await JobHunterAPI.registerUser(signupData);
+    setToken(token);
+  };
+
+  if (!currentUser.infoLoaded) return <div>Loading...</div>;
 
   return (
-    <>
+    <UserContext.Provider
+      value={{
+        currentUser: currentUser.data,
+        setCurrentUser,
+      }}
+    >
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <NavBar logout={logout} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </UserContext.Provider>
+  );
 }
 
-export default App
+export default App;
