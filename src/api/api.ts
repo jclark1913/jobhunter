022bfd26622fe909c8e571288648c41fa78b4aff
Justin Clark <jclarksummit@gmail.com>
2024-02-20 10:35:32 -0500

    Update env variable import for vite

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "https://justin-clark-jobly.onrender.com"
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 console.log("BASE_URL", BASE_URL);
 
 // interface ApiResponse {
