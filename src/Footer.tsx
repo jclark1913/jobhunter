c9b11360909c959d7ef099eee4b1264174b347db
Justin Clark <jclarksummit@gmail.com>
2024-02-20 19:36:28 -0500

    Add basic footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div className="text-red-700 z-12">Footer</div>
-  )
-}
+  const currentYear = new Date().getFullYear();
 
-export default Footer
+  return (
+    <div className="text-slate-50 z-12 mt-24 border-t p-2 border-gray-500 border-opacity-45 flex justify-center">
+      <small>
+        JobHunter © {currentYear} | Built by{" "}
+        <a href="https://github.com/jclark1913">Justin Clark</a>
+      </small>
+    </div>
+  );
+};
+
+export default Footer;
