e97ad20fa0aeb82579ca2fb01da40f9bc62ca88b
Justin Clark <jclarksummit@gmail.com>
2024-03-03 12:09:42 -0500

    Begin adding utility classes for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -9,6 +10,19 @@ module.exports = {
       fontFamily: {
         'sans': ['Inter', 'sans-serif'],
       },
+      backgroundImage: {
+        'hero-background': "url('/src/assets/background-image.jpg')",
+      },
+      colors: {
+        sitebackground: "rgba(var(--site-background))",
+        primarytext: "rgba(var(--primary-text))",
+        secondarytext: "rgba(var(--secondary-text))",
+        gradientstart: "rgba(var(--gradient-start))",
+        gradientend: "rgba(var(--gradient-end))",
+        normalborder: "rgba(var(--normal-border))",
+        hoverborder: "rgba(var(--hover-border))",
+        textfooter: "rgba(var(--text-footer))",
+      }
     },
   },
   plugins: [],
