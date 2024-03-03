2d90c1f96f66012f494a23a4411b77ffb376fce1
Justin Clark <jclarksummit@gmail.com>
2024-03-03 12:11:26 -0500

    Update styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -30,9 +30,9 @@ const NavBar: React.FC<NavBarProps> = ({ logout }) => {
     );
   };
   return (
-    <nav className="border-b border-opacity-50 border-gray-500 bg-[#23272F] text-slate-50">
+    <nav className="border-b border-opacity-50 border-gray-500 text-slate-50">
       <div className="flex container lg:py-4 px-4 flex-wrap items-center justify-between mx-auto">
-        <Link className="text-2xl text-transparent font-extrabold bg-clip-text bg-gradient-to-r from-slate-400 to-blue-600" to="/">
+        <Link className="text-2xl text-transparent font-extrabold bg-clip-text bg-gradient-to-r from-[#113587] to-[#0a65ff]" to="/">
           JobHunter
         </Link>
         {currentUser ? loggedInNavBar() : loggedOutNavBar()}
