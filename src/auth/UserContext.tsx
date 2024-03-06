d5d77b815094258591cd77474c811140088e7be1
Justin Clark <jclarksummit@gmail.com>
2024-03-06 15:27:18 -0500

    move directory and update imports

--- src/auth/UserContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-interface User {
-    [key: string]: any;
-}
-
-const UserContext = createContext({} as User); // Might want to refactor and just use a default value?
-
-export default UserContext;
