cc0ac20cd76b760393969e88ed553398d402cc05
Justin Clark <jclarksummit@gmail.com>
2024-03-06 16:07:25 -0500

    Add basic components for companies

--- src/components/companies/CompanyCard.tsx
+++ src/components/companies/CompanyCard.tsx
@@ -1,2 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+const CompanyCard: React.FC = ({ name, description, logoUrl, handle }) => {
+  return (
+    <Link to={`/companies/${handle}`}>
+      <div className="flex flex-row gap-4">
+        <h6>
+          {name} {logoUrl && <img src={logoUrl} alt={name} className="" />}
+        </h6>
+        <img src={logoUrl} alt={name} />
+        <div>
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default CompanyCard;
--- src/components/companies/CompanyList.tsx
+++ src/components/companies/CompanyList.tsx
@@ -1,33 +1,39 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 // import SearchForm from "../common/SearchForm";
 import { JobHunterAPI } from "../../api/api";
-// import CompanyCard from "./CompanyCard";
-
+import CompanyCard from "./CompanyCard";
 
 const CompanyList: React.FC = () => {
-    const [companies, setCompanies] = useState<any[]>([]);
+  const [companies, setCompanies] = useState<any[]>([]);
 
-    useEffect(function getCompaniesOnMount() {
-        search();
-    }, []);
+  useEffect(function getCompaniesOnMount() {
+    search();
+  }, []);
 
-    const search = async (name?: string) => {
-        const companies = await JobHunterAPI.getCompanies(name);
-        setCompanies(companies);
-    }
+  const search = async (name?: string) => {
+    const companies = await JobHunterAPI.getCompanies(name);
+    setCompanies(companies);
+  };
 
-    if (!companies) return <p>Loading...</p>;
+  if (!companies) return <p>Loading...</p>;
 
-    return (
-        <div>
-            <h1>Companies</h1>
-            {/* <SearchForm searchFor={search} /> */}
-            {companies.map((c) => (
-                <p key={c.handle}> {c.handle} </p>
-            ))}
-        </div>
-    );
-
-}
+  return (
+    <div>
+      <h1>Companies</h1>
+      <div className="flex flex-col align-middle items-left">
+        {/* <SearchForm searchFor={search} /> */}
+        {companies.map((c) => (
+          <CompanyCard
+            key={c.handle}
+            name={c.name}
+            description={c.description}
+            logoUrl={c.logoUrl}
+            handle={c.handle}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CompanyList;
