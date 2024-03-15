8f6a3a9d16b4ae40b9661faaa6d618c6ee0c32f9
Justin Clark <jclarksummit@gmail.com>
2024-03-15 14:31:45 -0400

    Update the RoutesList with new company routes

--- src/components/routes/RoutesList.tsx
+++ src/components/routes/RoutesList.tsx
@@ -3,8 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import { LoginData, SignUpData } from "../../App";
 import { Navigate } from "react-router-dom";
 import Homepage from "../Homepage";
-import LoginForm from "../../auth/LoginForm";
-import SignupForm from "../../auth/SignupForm";
+import LoginForm from "../auth/LoginForm";
+import SignupForm from "../auth/SignupForm";
+import CompanyList from "../companies/CompanyList";
 
 interface RoutesListProps {
   login: (loginData: LoginData) => Promise<void>;
@@ -20,15 +21,15 @@ const RoutesList: React.FC<RoutesListProps> = ({
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage />}/>
+        <Route path="/" element={<Homepage />} />
         {!currentUser ? (
           <>
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/login" element={<LoginForm login={login} />} />
+            <Route path="/signup" element={<SignupForm signup={signup} />} />
           </>
         ) : (
           <>
-            <Route path="/companies" />
+            <Route path="/companies" element={<CompanyList />} />
             <Route path="/companies/:handle" />
             <Route path="/jobs" />
             <Route path="/profile" />
