710e66b50c7604866c60407c7fc4ce0ea9d88e44
Justin Clark <jclarksummit@gmail.com>
2024-03-17 14:57:23 -0400

    Update link to homepage

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="text-slate-50 z-12 mt-24 border-t p-2 border-gray-500 border-opacity-45 flex justify-center">
       <small>
         JobHunter © {currentYear} | Built by{" "}
-        <a href="https://github.com/jclark1913">Justin Clark</a>
+        <a href="https://justinclark.bio">Justin Clark</a>
       </small>
     </div>
   );
