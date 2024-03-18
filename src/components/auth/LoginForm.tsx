db456b71270a351cb62d9a8d612162045ad1c82c
Justin Clark <jclarksummit@gmail.com>
2024-03-18 12:31:19 -0400

    Tweak util classes

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -40,14 +40,14 @@ const LoginForm: React.FC<LoginFormProps> = ({ login }) => {
         sequence={["Welcome back!", 3000]}
         cursor={true}
       /> */}
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-gradientstart to-[#0a65ff] text-4xl font-extrabold mb-10">
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-gradientstart to-gradientend text-4xl font-extrabold mb-10">
         Welcome back!
       </h1>
-      <div className="flex flex-col text-left justify-center align-middle items-center rounded-md border border-normalborder w-1/2 gap-3 bg-gradient-to-b from-white/5 to-[#040a24] shadow-l">
-        <h1 className="text-3xl font-extrabold pt-5">Log In</h1>
+      <div className="flex flex-col text-left justify-center align-middle items-center rounded-md border border-normalborder w-1/2 gap-3 bg-gradient-to-b from-white/5 to-sitebackground shadow-l">
+        <h1 className="text-3xl font-extrabold pt-5 text-primarytext">Log In</h1>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div className="flex flex-row align-middle items-center gap-4">
-            <VscAccount className="text-3xl" />
+            <VscAccount className="text-3xl text-primarytext" />
             <input
               type="text"
               placeholder="username"
@@ -56,11 +56,11 @@ const LoginForm: React.FC<LoginFormProps> = ({ login }) => {
               name="username"
               value={formData.username}
               onChange={handleChange}
-              className="bg-transparent border border-normalborder rounded-md p-2"
+              className="bg-transparent border border-normalborder rounded-md p-2 text-secondarytext"
             />
           </div>
           <div className="flex flex-row align-middle items-center gap-4">
-            <RiLockPasswordLine className="text-3xl" />
+            <RiLockPasswordLine className="text-3xl text-primarytext" />
             <input
               id="password"
               placeholder="password"
@@ -68,20 +68,20 @@ const LoginForm: React.FC<LoginFormProps> = ({ login }) => {
               type="password"
               value={formData.password}
               onChange={handleChange}
-              className="bg-transparent border border-normalborder rounded-md p-2"
+              className="bg-transparent border border-normalborder rounded-md p-2 text-secondarytext"
             />
           </div>
           <div className="flex justify-center gap-10">
             <button
               type="submit"
-              className="border border-normalborder rounded-md px-5 py-2 bg-inherit hover:bg-gradient-to-r from-gradientstart to-[#0a65ff] hover:text-white"
+              className="border text-primarytext border-normalborder rounded-md px-5 py-2 bg-inherit hover:bg-gradient-to-r from-gradientstart to-[#0a65ff] hover:text-white"
             >
               Log In
             </button>
           </div>
           <p className="text-sm text-center pb-3">
             <a
-              className="underline underline-offset-4 text-gray-500 hover:text-white"
+              className="underline underline-offset-4 text-secondarytext hover:text-primarytext"
               href="/signup"
             >
               Don&apos;t have an account? Click here to create one.
--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -48,16 +48,19 @@ const SignupForm: React.FC<SignupFormProps> = ({ signup }) => {
 
   return (
     <div className="flex flex-col align-middle justify-center items-center text-xl">
-      <TypeAnimation
+      {/* <TypeAnimation
         className="text-transparent bg-clip-text bg-gradient-to-r from-gradientstart to-[#0a65ff] text-4xl font-extrabold mb-10"
         sequence={["Let's get started!", 3000]}
         cursor={true}
-      />
-      <div className="flex flex-col text-left justify-center align-middle items-center rounded-md border border-normalborder w-1/2 gap-3 bg-gradient-to-b from-white/5 to-[#040a24] shadow-l">
-        <h1 className="text-3xl font-extrabold pt-5">Sign Up</h1>
+      /> */}
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-gradientstart to-gradientend text-4xl font-extrabold mb-10">
+        Let's get started!
+      </h1>
+      <div className="flex flex-col text-left justify-center align-middle items-center rounded-md border border-normalborder w-1/2 gap-3 bg-gradient-to-b from-white/5 to-sitebackground shadow-l">
+        <h1 className="text-3xl font-extrabold pt-5 text-primarytext">Sign Up</h1>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div className="flex flex-row align-middle items-center gap-4">
-            <VscAccount className="text-3xl" />
+            <VscAccount className="text-3xl text-primarytext" />
             <input
               type="text"
               placeholder="username"
@@ -66,11 +69,11 @@ const SignupForm: React.FC<SignupFormProps> = ({ signup }) => {
               name="username"
               value={formData.username}
               onChange={handleChange}
-              className="bg-transparent border border-normalborder rounded-md p-2"
+              className="bg-transparent border border-normalborder rounded-md p-2 text-secondarytext"
             />
           </div>
           <div className="flex flex-row align-middle items-center gap-4">
-            <RiLockPasswordLine className="text-3xl" />
+            <RiLockPasswordLine className="text-3xl text-primarytext" />
             <input
               id="password"
               placeholder="password"
@@ -78,11 +81,11 @@ const SignupForm: React.FC<SignupFormProps> = ({ signup }) => {
               type="password"
               value={formData.password}
               onChange={handleChange}
-              className="bg-transparent border border-normalborder rounded-md p-2"
+              className="bg-transparent border border-normalborder rounded-md p-2 text-secondarytext"
             />
           </div>
           <div className="flex flex-row align-middle items-center gap-4">
-            <BsPersonBoundingBox className="text-3xl" />
+            <BsPersonBoundingBox className="text-3xl text-primarytext" />
             <input
               id="firstName"
               placeholder="First name (Optional)"
@@ -90,11 +93,11 @@ const SignupForm: React.FC<SignupFormProps> = ({ signup }) => {
               type="text"
               value={formData.firstName}
               onChange={handleChange}
-              className="bg-transparent border border-normalborder rounded-md p-2"
+              className="bg-transparent border border-normalborder rounded-md p-2 text-secondarytext"
             />
           </div>
           <div className="flex flex-row align-middle items-center gap-4">
-            <CgDetailsLess className="text-3xl" />
+            <CgDetailsLess className="text-3xl text-primarytext" />
             <input
               id="lastName"
               placeholder="Last name (Optional)"
@@ -102,11 +105,11 @@ const SignupForm: React.FC<SignupFormProps> = ({ signup }) => {
               type="text"
               value={formData.lastName}
               onChange={handleChange}
-              className="bg-transparent border border-normalborder rounded-md p-2"
+              className="bg-transparent border border-normalborder rounded-md p-2 text-secondarytext"
             />
           </div>
           <div className="flex flex-row align-middle items-center gap-4">
-            <MdOutlineEmail className="text-3xl" />
+            <MdOutlineEmail className="text-3xl text-primarytext" />
             <input
               id="email"
               placeholder="Email"
@@ -114,20 +117,20 @@ const SignupForm: React.FC<SignupFormProps> = ({ signup }) => {
               type="email"
               value={formData.email}
               onChange={handleChange}
-              className="bg-transparent border border-normalborder rounded-md p-2"
+              className="bg-transparent border border-normalborder rounded-md p-2 text-secondarytext"
             />
           </div>
           <div className="flex justify-center gap-10">
             <button
               type="submit"
-              className="border border-normalborder rounded-md px-5 py-2 bg-inherit hover:bg-gradient-to-r from-gradientstart to-[#0a65ff] hover:text-white"
+              className="border border-normalborder rounded-md text-secondarytext px-5 py-2 bg-inherit hover:bg-gradient-to-r from-gradientstart to-gradientend"
             >
               Sign Up
             </button>
           </div>
           <p className="text-sm text-center pb-3">
             <a
-              className="underline underline-offset-4 text-gray-500 hover:text-white"
+              className="underline underline-offset-4 text-secondarytext hover:text-primarytext"
               href="/login"
             >
               Already have an account? Click here to sign in.
